059b5cc49bbd763cfa52e6dff9a3ee88d4654cc0
bmd007 <bmd579@gmail.com>
2018-10-04 20:44:49 +0200

    1233541

--- src/main/java/ir/tiroon/battleshipi/Main.java
+++ src/main/java/ir/tiroon/battleshipi/Main.java
@@ -6,7 +6,20 @@ import rpi.sensehat.api.dto.Color;
 public class Main {
 
     public static void main(String[] args) {
-        Screen s = new ScoreScreen();
-        s.showGlobeSight();
+//        Screen s = new ScoreScreen();
+//        s.showGlobeSight();
+
+        Point p = new Point(5,5,Color.RED);
+        p.startBlinking();
+        Point p2  = p;
+        SenseHatUtil.waitFor(8000);
+        p2.stopBlinking();
+        SenseHatUtil.waitFor(4000);
+        p2.startBlinking();
+        SenseHatUtil.waitFor(8000);
+        Point p3 = new Point(6,7,Color.GREEN);
+        p3.startBlinking();
+        p2 = p3;
+        p2.stopBlinking();
     }
 }
