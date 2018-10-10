c6dd34fdf656edbca8ddb85a66d5ebaf02ca1f7a
bmd007 <bmd579@gmail.com>
2018-10-10 17:06:08 +0200

    90% test

--- src/main/java/ir/tiroon/battleshipi/Main.java
+++ src/main/java/ir/tiroon/battleshipi/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("args:"+args[0]+":"+args[1]+":"+args[2]);
+        System.out.println("args:"+args[0]+":"+args[1]);
         try {
-            MQTTUtil.MQTTUtilConnect(args[2]);
+            MQTTUtil.MQTTUtilConnect(args[1]);
         } catch (Exception e) {
             e.printStackTrace();
         }
